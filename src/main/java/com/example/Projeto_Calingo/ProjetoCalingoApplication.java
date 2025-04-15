390b5cf0d1badf235c0db34549de9d5d870935fd
Orlando <orlandotelles129@gmail.com>
2025-04-15 19:38:32 -0300

    primeiro commit

---
+++ src/main/java/com/example/Projeto_Calingo/ProjetoCalingoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Projeto_Calingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCalingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCalingoApplication.class, args);
+	}
+
+}
